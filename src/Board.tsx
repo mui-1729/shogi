24df296b3d850dacddb0895157cefd3c3fa46df4
mui-1729 <s1f102503015@iniad.org>
2025-05-07 23:08:04 +0900

    '動作'

--- src/Board.tsx
+++ src/Board.tsx
@@ -12,11 +12,11 @@ const initialBoard: Piece[][] = [
   ],
   [
     null, { type: '飛', player: 1 }, null, null, null, null, null, { type: '角', player: 1 }, null],
-    Array(9).fill({ type: '歩', player: 1 }),
+    Array.from({ length: 9}, () => ({type: '歩', player: 1})),
     Array(9).fill(null), 
     Array(9).fill(null), 
     Array(9).fill(null), 
-    Array(9).fill({ type: '歩', player: 2 }),
+    Array.from({ length: 9}, () => ({type: '歩', player: 2})),
   
   [
     null, { type: '角', player: 2 }, null, null, null, null, null, { type: '飛', player: 2 }, null
@@ -33,24 +33,60 @@ const Board: React.FC = () => {
     const [select, setselect] = useState <Position> (null);
     const [nowplayer, setnowplayer] = useState<number>(1);
 
-    const validMove = (
-        from: Position,
-        to: Position,
-        piece: Piece,
-    ): boolean => {
-        if (!from || !to || !piece ) return false;
-        if (piece.player !== nowplayer) return false;
-        
-        if (piece.type === '歩') {
-          if (nowplayer === 1){
-            return to!.row === from.row - 1 && to!.col === from.col;
-          } else {
-            return to!.row === from.row + 1 && to!.col === from.col;
-          }
-          }
-      
-        return true;
-      };
+    type Delta = { dr: number; dc: number };
+
+const canMove: Record<string, Delta[]> = {
+  歩:   [{ dr: 1, dc: 0 }],
+  桂:   [{ dr: 2, dc: -1 }, { dr: 2, dc: 1 }],
+  銀:   [
+    { dr: 1, dc: 0 }, { dr: 1, dc: -1 }, { dr: 1, dc: 1 },
+    { dr: -1, dc: -1 }, { dr: -1, dc: 1 }
+  ],
+  金:   [
+    { dr: 1, dc: 0 }, { dr: 0, dc: -1 }, { dr: 0, dc: 1 },
+    { dr: -1, dc: 0 }, { dr: 1, dc: -1 }, { dr: 1, dc: 1 }
+  ],
+  王:   [
+    { dr: 1, dc: 0 }, { dr: -1, dc: 0 },
+    { dr: 0, dc: 1 }, { dr: 0, dc: -1 },
+    { dr: 1, dc: 1 }, { dr: 1, dc: -1 },
+    { dr: -1, dc: 1 }, { dr: -1, dc: -1 }
+  ],
+  玉:   [
+    { dr: 1, dc: 0 }, { dr: -1, dc: 0 },
+    { dr: 0, dc: 1 }, { dr: 0, dc: -1 },
+    { dr: 1, dc: 1 }, { dr: 1, dc: -1 },
+    { dr: -1, dc: 1 }, { dr: -1, dc: -1 }
+  ],
+  香:   [
+    { dr: 1, dc: 0 }, { dr: 2, dc: 0 },
+    { dr: 3, dc: 0 }, { dr: 4, dc: 0 },
+    { dr: 5, dc: 0 }, { dr: 6, dc: 0 },
+    { dr: 7, dc: 0 }, { dr: 8, dc: 0 }
+  ],
+};
+
+function validMove(
+    from: Position,
+    to: Position,
+    piece: Piece
+  ): boolean {
+    if (!from || !to || !piece) return false;
+    if (piece.player !== nowplayer) return false;
+  
+    const dir = piece.player === 1 ? 1 : -1;
+
+    if (canMove[piece.type]) {
+      const dr = to.row - from.row;
+      const dc = to.col - from.col;
+  
+      return canMove[piece.type]!.some(delta =>
+        delta.dr * dir === dr && delta.dc === dc
+      );
+    }
+
+    return false;
+  }
 
     const click = (row: number, col: number) => {
         if (!select){
@@ -60,7 +96,6 @@ const Board: React.FC = () => {
         } else {
 
             if ( validMove(select, { row, col }, board[select.row][select.col])){
-                console.log("Checking move validity:", {nowplayer})
                 const newBoard = board.map((row) => row.slice());
                 newBoard[{ row, col }.row][{ row, col }.col] = board[select.row][select.col];
                 newBoard[select.row][select.col] = null;
